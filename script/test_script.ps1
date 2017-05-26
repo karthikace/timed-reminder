df67b9e5905fdd44a8748381428b5c5067d47c2a
Karthik Balasubramanian <karthikace@users.noreply.github.com>
2017-05-26 10:40:44 -0700

    Add files via upload

--- script/test_script.ps1
+++ script/test_script.ps1
@@ -1,4 +1,4 @@
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
 
 $mainform = New-Object System.Windows.Forms.Form 
@@ -10,14 +10,47 @@ $mainform.ShowInTaskbar = $false
 $mainform.TopMost = $true
 $mainform.KeyPreview = $true
 
+$subform = New-Object System.Windows.Forms.Form
+$subform.Size = New-Object System.Drawing.Size(300,150) 
+$subform.StartPosition = "CenterScreen"
+$subform.FormBorderStyle = "None"
+$subform.BackColor = "DarkOrange"
+$subform.ShowInTaskbar = $false
+$subform.TopMost = $true
+$subform.KeyPreview = $true
+$sublabel = New-Object System.Windows.Forms.Label
+$sublabel.Location = New-Object System.Drawing.Size(10,20) 
+$sublabel.Size = New-Object System.Drawing.Size(280,20) 
+$sublabel.Text = "Please enter the information in the space below:"
+$subform.Controls.Add($objLabel) 
+
+
 while($true)
 {
-    Start-Sleep -Seconds 3 #testing
+    Start-Sleep -Seconds 3
+
     
     $mainform.Add_KeyDown({
         if($_.KeyCode -eq "Enter")
         {
-         $form.Close()
+            #progressbar
+            $mainform.Dispose()
+        }
+    })
+    $mainform.Add_KeyDown({
+        if($_.KeyCode -eq "Escape")
+        {
+            $delay = $true
+            $count = 1
+            $mainform.Dispose()
+            while($delay)
+            {
+                Start-Sleep -Seconds 2
+                $subform.Text
+                $subform.ShowDialog()
+
+            }
+              
         }
     })
 
