f967685a58c7afbed6813c3b47f0a6f34017e40e
Karthik Balasubramanian <karthikace@users.noreply.github.com>
2017-05-25 20:10:57 -0700

    Add files via upload

---
+++ test_script.ps1
@@ -0,0 +1,26 @@
+﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
+$mainform = New-Object System.Windows.Forms.Form 
+$mainform.Text = "Reminder"
+$mainform.Size = New-Object System.Drawing.Size(300,150) 
+$mainform.StartPosition = "CenterScreen"
+$mainform.FormBorderStyle = "None"
+$mainform.ShowInTaskbar = $false
+$mainform.TopMost = $true
+$mainform.KeyPreview = $true
+
+while($true)
+{
+    Start-Sleep -Seconds 3
+
+    
+    $mainform.Add_KeyDown({
+        if($_.KeyCode -eq "Enter")
+        {
+         $form.Close()
+        }
+    })
+
+    $mainform.ShowDialog()
+}
